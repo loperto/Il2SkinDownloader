1ebfe0cb94a1a2aa7eaba70ce3c64b252e63c601
loperto <loperto88@gmail.com>
2019-07-26 00:07:08 +0200

    added project il2Game

---
+++ Il2.GreatBattles/Il2Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Il2.GreatBattles
+{
+    public class Il2Game
+    {
+        private readonly string _installationPath;
+
+        public Il2Game(string installationPath)
+        {
+            _installationPath = installationPath;
+        }
+
+        public IEnumerable<DirectoryInfo> GetCustomSkinDirectories()
+        {
+            return Directory.EnumerateDirectories(Path.Combine(_installationPath, @"data\graphics\skins")).Select(x => new DirectoryInfo(x));
+        }
+    }
+}
--- Il2.SkinDownloader.Tests/UnitTest1.cs
+++ Il2.SkinDownloader.Tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Il2.GreatBattles;
 using Il2.RemoteDrive.GoogleDrive;
 using NUnit.Framework;
 
@@ -13,8 +17,25 @@ namespace Il2.SkinDownloader.Tests
         [Test]
         public void Test1()
         {
+            var il2 = new Il2Game(@"Z:\Steam\steamapps\common\IL-2 Sturmovik Battle of Stalingrad");
+            var planes = il2.GetCustomSkinDirectories();
+            foreach (var plane in planes)
+            {
+                Console.WriteLine(plane.Name);
+            }
+        }
+
+        [Test]
+        public async Task Test2()
+        {
             var drive = new GoogleDrive("skinDownloader");
             drive.Connect(@"C:\Temp\auth.json");
+            var files = await drive.GetFiles(CancellationToken.None);
+            foreach (var googleDriveItem in files)
+            {
+                Console.WriteLine(googleDriveItem);
+            }
+
         }
     }
 }
