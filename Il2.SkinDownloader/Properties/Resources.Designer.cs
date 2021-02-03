da5ae1fb024f4dedb977ae07e93def14a85b1e8e
e.belbusti <e.belbusti@sitrap.it>
2021-02-03 15:30:04 +0100

    fix resources

--- Il2.SkinDownloader/Properties/Resources.Designer.cs
+++ Il2.SkinDownloader/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace Il2SkinDownloader.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap download1 {
+            get {
+                object obj = ResourceManager.GetObject("download1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap gemini {
+            get {
+                object obj = ResourceManager.GetObject("gemini", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap plane_green {
             get {
                 object obj = ResourceManager.GetObject("plane_green", resourceCulture);
